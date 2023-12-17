d60d4a1af994e84e142d4d8bceda1a72c9bcbe57
Dawid Wiechec <wiechec.dawid@gmail.com>
2023-12-17 01:01:02 +0100

    blink project add

---
+++ blink/blink.c
@@ -0,0 +1,16 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define BLINK_DELAY_MS 500
+
+int main() {
+    DDRB |= (1 << DDB5);
+    while(1){
+      PORTB |= (1 << PB5);
+      _delay_ms(BLINK_DELAY_MS);
+      PORTB &= ~(1 << PB5);
+      _delay_ms(BLINK_DELAY_MS);
+    }
+
+    return 0;
+}
